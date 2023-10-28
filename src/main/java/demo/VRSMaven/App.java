34b47583fd9604d134d4450ee3b52121eef7bb62
KethireddyRamya <107686467+KethireddyRamya@users.noreply.github.com>
2023-10-28 08:00:25 +0530

    First commit

---
+++ src/main/java/demo/VRSMaven/App.java
@@ -0,0 +1,13 @@
+package demo.VRSMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
